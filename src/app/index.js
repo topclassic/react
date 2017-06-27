43797772c8ecacdb31d06be57eafbe026c7b3378
Chotipat Poowongthanarat <top_classic@hotmail.com>
2017-06-27 16:10:03 +0700

    Multiple Components with sass

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-console.log("It works!");
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Your Name</h2>
+                <p>Chotipat</p>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<div> <App /> <App /> </div>,document.getElementById('MultiComp'))
